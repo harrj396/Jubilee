71e45637f89ea9c8c03e7114e478da1531f7f64a
kayce <justinross28@gmail.com>
2022-12-12 10:55:49 -0800

    completed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function ColorSchemesExample() {
   return (
     <>
-      <Navbar bg="light" variant="light">
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Jubilee</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#record">Make a Record</Nav.Link>
+            <Nav.Link href="#about">About Us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
