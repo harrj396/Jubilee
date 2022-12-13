93ce0d4b2de7355ec3723f234a1568ee9536028c
harrj396 <ahletmethink@gmail.com>
2022-12-13 10:59:42 -0500

    added getGenres and getTracks to appJs

--- client/src/App.js
+++ client/src/App.js
@@ -45,4 +45,23 @@ const APIController = (function() {
     const data = await result.json();
     return data.access_token;
   }
-})
+  const _getGenres = async (token) => {
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+});
+  
+  const data = await result.json();
+  return data.categories.items;
+
+  } 
+
+  const _getTracks = async (token, tracksEndPoint) => {
+    const limit = 10;
+    const result = await fetch (`${tracksEndPoint}?limit=$(limit)`, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+    });
+    const data = await result.json();
+    return data.items;
+  }
