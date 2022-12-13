732c2769ff34f9138dd4c2383f7b4560a7f06869
kayce <justinross28@gmail.com>
2022-12-13 08:01:50 -0800

    added login, signup

--- client/src/App.js
+++ client/src/App.js
@@ -13,15 +13,19 @@ function ColorSchemesExample() {
         <Container>
           <Navbar.Brand href="#home">Jubilee</Navbar.Brand>
           <Nav className="me-auto">
+
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#record">Make a Record</Nav.Link>
             <Nav.Link href="#about">About Us</Nav.Link>
+            
           </Nav>
+          <Button href='login' className='me-3' variant="light">Login</Button>{' '}
+          <Button href='signup' variant="light">Sign Up</Button>{' '}
         </Container>
       </Navbar>
       <img className='img1' src='/vinyll.jpg' height={420}></img>
       <h1 className='textover'>Design your Custom Vinyl today!</h1>
-      <Button className='d-flex justify-content-center' id='button1' href='#login' variant="primary">Get Started</Button>{' '}
+      <Button id='button1' href='signup' variant="primary">Get Started</Button>{' '}
     </>
   );
 }
