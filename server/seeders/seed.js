9fc396b3f9ba8f9da77bc2c6cc61076a8660e83b
oliverLo78 <oliverberto@gmail.com>
2022-12-14 12:19:28 -0500

    here is where I'm at jubilee

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,10 +1,10 @@
 const db = require('../config/connection');
-const { Thought } = require('../models');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const { Album } = require('../models');
+const albumSeeds = require('./albumSeeds.json');
 
 db.once('open', async () => {
-  await Thought.deleteMany({});
-  await Thought.create(thoughtSeeds);
+  await Album.deleteMany({});
+  await Album.create(AlbumSeeds);
 
   console.log('all done!');
   process.exit(0);
