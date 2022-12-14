bd261ed3e7267ae7acd81c96e60bc514897a3fdc
harrj396 <ahletmethink@gmail.com>
2022-12-14 13:48:40 -0500

    redid appjs

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import React, {useEffect } from 'react';
 
 function ColorSchemesExample() {
+
+  const spotify_client_Id = process.env.REACT_APP_SPOTFIY_CLIENT_ID;
+  const spotify_client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+  const authParameters = {
+    method: 'POST',
+    headers: {
+    'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials&client_id=' + spotify_client_Id + '&client_secret=' + spotify_client_secret
+  }
+  
+  
+  console.log('hey')
+  function ypp() {
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+    .then(result => result.json())
+    .then(data => console.log(data))
+    console.log('hello')
+    
+  }
+  
+  useEffect(() => {
+    ypp()
+  }, []);
+    
+   
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -29,5 +56,8 @@ function ColorSchemesExample() {
     </>
   );
 }
+
+
+  
 
 export default ColorSchemesExample;
